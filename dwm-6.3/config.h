426b2aeb8a42d5c485ef1c72f033bc1a3f90a3f4
Tymon <79712082+Tomcion@users.noreply.github.com>
2022-06-18 18:42:40 +0200

    Adding a systray to dwm and switching from the fullgaps patch to ru_gaps

--- dwm-6.3/config.h
+++ dwm-6.3/config.h
@@ -2,10 +2,15 @@
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int gappx     = 10;        /* gaps between windows */
+static const int gappx     = 5;                 /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;     /* 0 means no systray */
+static const int showbar            = 1;     /* 0 means no bar */
+static const int topbar             = 1;     /* 0 means bottom bar */
 static const char *fonts[]          = { "Hack Nerd Font:size=10" };
 static const char dmenufont[]       = "Hack Nerd Font:size=10";
 static const char col_gray1[]       = "#222222";
@@ -42,9 +47,9 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+    { "[]=",      tile },    /* first entry is default */
+    { "><>",      NULL },    /* no layout function means floating behavior */
+    { "[M]",      monocle },
 };
 
 /* key definitions */
@@ -64,10 +69,14 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *browsercmd[]  = { "firefox", NULL };
 
-static const char *volume_up[] = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%", NULL};
-static const char *volume_down[] = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%", NULL};
-static const char *volume_mute[] = { "pactl", "set-sink-mute", "@DEFAULT_SINK@", "toggle", NULL};
+/* static const char *volume_up[] = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%", NULL}; */
+/* static const char *volume_down[] = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%", NULL}; */
+/* static const char *volume_mute[] = { "pactl", "set-sink-mute", "@DEFAULT_SINK@", "toggle", NULL}; */
+static const char *volume_up[] = { "dwmblocks_volume", "up", NULL};
+static const char *volume_down[] = { "dwmblocks_volume", "down", NULL};
+static const char *volume_mute[] = { "dwmblocks_volume", "mute", NULL};
 
+#include "movestack.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
@@ -76,8 +85,8 @@ static Key keys[] = {
 	{ 0,    XF86XK_AudioLowerVolume,           spawn,          {.v = volume_down } },
 	{ 0,    XF86XK_AudioRaiseVolume,           spawn,          {.v = volume_up } },
 	{ 0,    XF86XK_AudioMute,                  spawn,          {.v = volume_mute } },
-	{ 0,    XF86XK_MonBrightnessUp,            spawn,          SHCMD("light -A 10") },
-	{ 0,    XF86XK_MonBrightnessDown,          spawn,          SHCMD("light -U 10") },
+	{ 0,    XF86XK_MonBrightnessUp,            spawn,          SHCMD("dwmblocks_brightness up") },
+	{ 0,    XF86XK_MonBrightnessDown,          spawn,          SHCMD("dwmblocks_brightness down") },
 	{ 0,    XF86XK_KbdBrightnessUp,            spawn,          SHCMD("kbd_light -u") },
 	{ 0,    XF86XK_KbdBrightnessDown,          spawn,          SHCMD("kbd_light -d") },
 	{ MODKEY|ShiftMask,             XK_s,      spawn,          SHCMD("screenshot") },
@@ -88,6 +97,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_u,      incnmaster,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
@@ -96,7 +107,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
+    { MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_f,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
@@ -105,9 +116,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
- 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
- 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
- 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0 } },
+ 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -5 } },
+ 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +5 } },
+ 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
--- st-0.8.5/config.h
+++ st-0.8.5/config.h
@@ -212,6 +212,8 @@ ResourcePref resources[] = {
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
+	{ NULL,                 Button4, kscrollup,      {.i = 1} },
+	{ NULL,                 Button5, kscrolldown,    {.i = 1} },
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
 	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
 	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
@@ -237,6 +239,10 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ TERMMOD,              XK_k,           kscrollup,      {.i = -1} },
+	{ TERMMOD,              XK_j,           kscrolldown,    {.i = -1} },
+    { ControlMask,          XK_k,           kscrollup,      {.i = +1} },
+	{ ControlMask,          XK_j,           kscrolldown,    {.i = +1} },
 };
 
 /*
